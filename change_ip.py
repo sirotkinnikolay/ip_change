13262d8c0f7ea71859bf8f9592ac9f69a600272f
sirotkin.nikola@mail.ru <sirotkin.nikola@mail.ru>
2023-11-12 02:13:45 +0300

    save

--- change_ip.py
+++ change_ip.py
@@ -3,6 +3,7 @@ import time
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
 
 command = './start-tor-browser.desktop'
@@ -15,6 +16,11 @@ with subprocess.Popen(command, cwd='/home/vboxuser/Downloads/tor-browser', stdou
         options.add_argument('--disable-dev-shm-usage')
         options.add_argument('--proxy-server=%s' % PROXY)
         with webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options) as driver:
+            driver.implicitly_wait(10)
             driver.get("https://2ip.ru/")
-            time.sleep(20000)
+            button = driver.find_element(By.XPATH, '/html/body/div[1]/div/p[3]/a[2]')
+            button.click()
+            you_ip = driver.find_element(By.XPATH, '/ html/body/div[1]/div[3]/div[1]/div/div[1]/section[1]/div/div['
+                                                   '1]/div/div[1]/span')
+            print(f'Your IP to log in to the network -------> {you_ip.text}')
             driver.close()
