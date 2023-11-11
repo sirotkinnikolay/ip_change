f6a8f5fa35aab0b0b2229f5fd43879d232d76f91
sirotkin.nikola@mail.ru <sirotkin.nikola@mail.ru>
2023-11-12 01:58:45 +0300

    save

--- change_ip.py
+++ change_ip.py
@@ -1,22 +1,20 @@
 import subprocess
 import time
-
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 
-
 command = './start-tor-browser.desktop'
 with subprocess.Popen(command, cwd='/home/vboxuser/Downloads/tor-browser', stdout=subprocess.PIPE) as proc:
     time.sleep(5)
     while True:
-        PROXY = "socks5://localhost:9050"
+        PROXY = "socks5://127.0.0.1:9150"
         options = Options()
         options.add_argument('--no-sandbox')
         options.add_argument('--disable-dev-shm-usage')
         options.add_argument('--proxy-server=%s' % PROXY)
         with webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options) as driver:
             driver.get("https://2ip.ru/")
-
+            time.sleep(20000)
             driver.close()
