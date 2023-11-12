f33c50b6c0083a99e44799f011fe2bf53c20a99b
sirotkin.nikola@mail.ru <sirotkin.nikola@mail.ru>
2023-11-12 22:59:20 +0300

    save

--- README.md
+++ README.md
@@ -1,9 +1,11 @@
-## Скрипт для изменения IP-адреса.  
-# Версия операционной системы: Ubuntu 22.04.3 LTS 64-bit  
+# Скрипт для замены PROXY-сервера и дальнейшего его использования для парсинга с помощью selenium.  
+## Версия операционной системы: Ubuntu 22.04.3 LTS 64-bit  
 Установите из официальных источников браузер GOOGLE CHROME https://www.google.ru/intx/ru/chrome/  
 и TOR BROWSER https://www.torproject.org/download/
-# В терминале запустите команды:  
+## В терминале запустите команды:  
 
 ``apt install python3-pip``  
 ``sudo apt update``  
 ``pip install selenium webdriver-manager``  
+
+Укажите в переменной ``tor_browser_storage_location`` место хранения TOR BROWSER
--- change_ip.py
+++ change_ip.py
@@ -7,20 +7,19 @@ from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
 
 command = './start-tor-browser.desktop'
-with subprocess.Popen(command, cwd='/home/vboxuser/Downloads/tor-browser', stdout=subprocess.PIPE) as proc:
+tor_browser_storage_location = '/home/vboxuser/Downloads/tor-browser'
+with subprocess.Popen(command, cwd=tor_browser_storage_location, stdout=subprocess.PIPE) as proc:
     time.sleep(5)
-    while True:
-        PROXY = "socks5://127.0.0.1:9150"
-        options = Options()
-        options.add_argument('--no-sandbox')
-        options.add_argument('--disable-dev-shm-usage')
-        options.add_argument('--proxy-server=%s' % PROXY)
-        with webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options) as driver:
-            driver.implicitly_wait(10)
-            driver.get("https://2ip.ru/")
-            button = driver.find_element(By.XPATH, '/html/body/div[1]/div/p[3]/a[2]')
-            button.click()
-            you_ip = driver.find_element(By.XPATH, '/ html/body/div[1]/div[3]/div[1]/div/div[1]/section[1]/div/div['
-                                                   '1]/div/div[1]/span')
-            print(f'Your IP to log in to the network -------> {you_ip.text}')
-            driver.close()
+    PROXY = "socks5://127.0.0.1:9150"
+    options = Options()
+    options.add_argument('--no-sandbox')
+    options.add_argument('--disable-dev-shm-usage')
+    options.add_argument('--proxy-server=%s' % PROXY)
+    with webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options) as driver:
+        driver.implicitly_wait(10)
+        driver.get("https://2ip.ru/")
+        button = driver.find_element(By.XPATH, '/html/body/div[1]/div/p[3]/a[2]')
+        button.click()
+        you_ip = driver.find_element(By.XPATH, '/ html/body/div[1]/div[3]/div[1]/div/div[1]/section[1]/div/div['
+                                               '1]/div/div[1]/span')
+        print(f'Your IP to log in to the network -------> {you_ip.text}')
